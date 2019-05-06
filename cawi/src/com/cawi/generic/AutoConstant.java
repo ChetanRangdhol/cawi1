28ae65daecc57c3456f2041508ed1bd8b18df3ef
Chetan <Chetan@DESKTOP-GJ6MLLF>
2019-05-06 14:27:46 +0530

    first

---
+++ cawi/src/com/cawi/generic/AutoConstant.java
@@ -0,0 +1,11 @@
+package com.cawi.generic;
+
+public interface AutoConstant
+{
+	
+	String chrome_key ="webdriver.chrome.driver";
+	String gecko_key="webdriver.gecko.driver";
+	String gecko_value="./drivers/geckodriver.exe";
+	String chrome_value="./drivers/chromedriver.exe";
+	String File_Path="./data/input.xlsx";
+}
---
+++ cawi/src/com/cawi/generic/baseTest.java
@@ -0,0 +1,40 @@
+package com.cawi.generic;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
+
+public abstract class baseTest implements AutoConstant 
+{
+
+	public WebDriver driver;
+	@BeforeMethod
+	public void precondition()
+	{
+		System.setProperty(chrome_key,chrome_value);
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		driver.get("https://cawi.mobinius.ai/");
+	}
+	@AfterMethod
+	public void postcondition(ITestResult res)
+	{
+		int status = res.getStatus();
+		if(status==2)
+		{
+			String name =res.getName();
+			Reporter.log("failed method "+name, true);
+			genericUtils.getScreenShot(driver,name);
+		}
+		driver.quit();
+	}
+}
